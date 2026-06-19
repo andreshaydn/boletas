0778d2db4c7569f09fed37eade47538c25107018
andreshaydn <115517134+andreshaydn@users.noreply.github.com>
2026-06-19 17:35:38 -0400

    Fix file paths to use consistent casing

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'boleta-2026-v1';
-const FILES = ['/boletas/', '/Boletas/index.html', '/Boletas/manifest.json'];
+const FILES = ['/boletas/', '/boletas/index.html', '/boletas/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
